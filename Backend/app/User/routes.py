cfa4cc15def7873bc388b6ebba543eb92734ea3c
Varun Shanbhag <vshanbhag1@hawk.iit.edu>
2020-10-17 00:06:43 -0500

    Clean up

--- Backend/app/User/routes.py
+++ Backend/app/User/routes.py
@@ -5,7 +5,6 @@ from flask import jsonify, make_response, abort
 @bp.route('/login/<email>', methods = ['GET'])
 def login(email):
     data = { "email": email }
-    #Validate data
     user = User(data)
     if user.find is None:
         abort(404)
