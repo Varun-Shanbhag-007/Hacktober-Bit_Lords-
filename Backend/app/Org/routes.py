19384952b4cb08563474f328cd030a78da1c715b
Varun Shanbhag <vshanbhag1@hawk.iit.edu>
2020-10-17 05:39:16 -0500

    Save org data API

--- Backend/app/Org/routes.py
+++ Backend/app/Org/routes.py
@@ -1,11 +1,11 @@
 from app.Org import bp
 from Models.Org import Org
-from flask import jsonify, make_response, abort
+from flask import jsonify, make_response, abort,request
 
-@bp.route('/addData', methods = ['POST'])
+@bp.route('/org/addOrgData/', methods = ['POST'])
 def saveData():
-    data = {}
+    val = request.get_json(force=True)
+    data = val
     org = Org(data)
     org.save()
-   
     return make_response(jsonify({"status":"success"}), 200)
