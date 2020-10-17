48dcec29502a2aab373164a93e0c5df7df564054
Varun Shanbhag <vshanbhag1@hawk.iit.edu>
2020-10-17 17:17:39 -0500

    Added Get for Org

--- Backend/Models/Org.py
+++ Backend/Models/Org.py
@@ -3,3 +3,7 @@ import Utility.dbController as dbController
 
 class Org(MainModel):
     db_collection = dbController.connect()["org"]
+
+    @property
+    def find(self):
+        return self.db_collection.find_one({"org_key": self.org_key})
--- Backend/app/Org/routes.py
+++ Backend/app/Org/routes.py
@@ -1,11 +1,25 @@
 from app.Org import bp
 from Models.Org import Org
 from flask import jsonify, make_response, abort,request
+from bson.json_util import dumps
 
-@bp.route('/org/addOrgData/', methods = ['POST'])
+@bp.route('/org/addOrgData', methods = ['POST'])
 def saveData():
     val = request.get_json(force=True)
     data = val
     org = Org(data)
     org.save()
     return make_response(jsonify({"status":"success"}), 200)
+
+
+@bp.route('/org/getOrg/<email>')
+def getOrg(email):
+    print(email)
+    data = {"org_key": "asdgj@df.com" }
+    org = Org(data)
+    val = org.find
+    print(val)
+    if val is None:
+        abort(404)
+    else:
+        return make_response(dumps(val), 200)
