8715d4a9d53b45f9abe04e758ebfd3066098dd82
naunidhsingh13 <nsingh28@hawk.iit.edu>
2020-10-18 00:58:32 -0500

    Added WordedAssoc file

---
+++ Backend/app/Engine/OrgSearch/WordAssoc.py
@@ -0,0 +1,18 @@
+
+Domain_Words_Map = {"Family, Children, and Survivors" : {"family", "children", "kids", "survivors", "wife", "mother", "father"},
+"Women Veterans" : {"woman veteran", "Women Veterans", "special features", "women health care", "women veteran health care", "abuse awareness", "domestic violance", "abuse awareness and prevention"},
+"Employment & Job Training" : {"job", "employement", "job opportunity", "salary", "renumeration"},
+"Legal Support" : {"advocate", "court", "judge", "law", "settlement", "suit", "law suit"},
+"Education" : {"school", "college", "university", "student", "research"},
+"Homelessness & Housing" : {"loan", "shelter", "home loan", "addiction treatment"},
+"Benefits" : {"tax", "unemployment", "benefits", "senior", "health benefits", "health insurance"},
+"Emergency Financial Assistance" : {"financial assistance", "funds", "loan", "relief fund"},
+"Financial Literacy" : {"financial literacy", "budget", "budget planning", "retirement", "risk management", "savings", "debt"},
+"Behavioral Health/Mental Health" : {"health", "mental", "support", "health insurance", "psychiatrist", "psychology", "psychologist"},
+"Food Insecurity" : {"food", "food insecurity", "healthy food", "nutrition", "shortage of food"},
+"Transportation" : {"transit", "transportation", "public transport"},
+"Medical" : {"hospitals", "health", "medical insurance", "aid", "medical aid"},
+"Dental" : {"dental", "teeth", "root canal", "dental insurance"}
+}
+
+
