deeec325901b7bb14e34d0b9d28a261525c3a76a
tushar-ntiave <tushar.nitave@gmail.com>
2020-10-17 21:36:26 -0500

    handled request for update and save query

--- Backend/Models/Org.py
+++ Backend/Models/Org.py
@@ -5,5 +5,13 @@ class Org(MainModel):
     db_collection = dbController.connect()["org"]
 
     @property
+    def save(self):
+        return self.db_collection.insert(self)
+
+    @property
     def find(self):
         return self.db_collection.find_one({"org_key": self.org_key})
+
+    @property
+    def update(self):
+        return self.db_collection.update_one({"org_key": self.org_key} , {"$set":self})
--- Backend/app/Org/routes.py
+++ Backend/app/Org/routes.py
@@ -6,9 +6,15 @@ from bson.json_util import dumps
 @bp.route('/org/addOrgData', methods = ['POST'])
 def saveData():
     val = request.get_json(force=True)
-    data = val
+    data = {"org_key": val["org_key"]}
     org = Org(data)
-    org.save()
+    if org.find:
+        query = Org(val)
+        result = query.update
+    else:
+        query = Org(val)
+        result = query.save
+
     return make_response(jsonify({"status":"success"}), 200)
 
 
